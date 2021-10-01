6acced9a6004bc9ec97e505db0c061d31ca20d45
Jen Beckett ICS 226 <jennybeckett2@hotmail.com>
2021-09-30 19:53:55 -0700

    working post refactor

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import socket
-#import sys
 
-
+#this function takes the reply generated from the program, and sends it over to the client via TCP.
+#the param reply defaults to either NO (put command) or \n (get command). Sends OK if PUT command was succesful,
+#OR the retrieved message if the GET commmand was succesful
 def send_reply(reply):
     client.sendall(reply) # Destination IP and port implicit due to accept call
     client.close() # Termination
-    
 
+#this function returns the KEY for locating the message. Takes no params as fullS is a global variable (uh oh)
+#no changes are made to the orig string.    
+def get_message_key_from_fullString():
+    return fullS[3 :11]
 
 BUF_SIZE = 1024
 HOST = ''
@@ -18,90 +22,74 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # More on this later
 sock.bind((HOST, PORT)) # Claim messages sent to port "PORT"
 sock.listen() # Enable server to receive 1 connection at a time
 messageDict = None
+
 while True:
     print("Waiting to recieve message from client.")
     client, sockname = sock.accept() # Wait until a connection is established
     print('Client:', client.getpeername()) # Destination IP and port
     data = client.recv(BUF_SIZE) # recvfrom not needed since address is known
     fullS = data.decode()
-    print(fullS + " is the recieved string")
-    goodKey = False
-    goodMessage = False
-
-
     
-    
+    reply = ("NO\n").encode('utf-8')
     command = fullS[0:3]
-    try:   
+    try:
+        ########## START PUT COMMAND ##########   
         if command == "PUT" or command == "put":
-            messageKey = fullS[3 :11]
+            messageMax = 11
+
+            messageKey = get_message_key_from_fullString()
             if (messageKey.isalnum() == False):
-                reply = ("NO\n").encode('utf-8')
                 #reply = ("NO! Please use only alphanum chars for key.\n").encode('utf-8')
                 send_reply(reply) 
-            elif len(fullS) < 12: #a valid message will have 13chars before the message starts.
-                reply = ("NO\n").encode('utf-8')
+            elif len(fullS) < messageMax: #a valid message will have 12chars before the message starts.
                 #reply = ("No, command and key too short \n").encode('utf-8')
                 send_reply(reply)
-                command = "bad"
+                #command = "bad"
             else:
-                goodKey = True
+                savedMessage = fullS[messageMax:-1] #a valid message will have 12chars before the message starts.
 
-            savedMessage = fullS[11:-1] #a valid message will have 12chars before the message starts.
-            if len(savedMessage) > 160: #if user enters a message longer than 160 bytes in length, truncates
-                savedMessage = savedMessage[0: 160]
-                reply = ("NO\n").encode('utf-8')
+            
+
+            maxMessageInBytes = 160
+            if len(savedMessage) > maxMessageInBytes: #if user enters a message longer than 160 bytes in length, rejects 
                 #reply = ("No, message too long\n").encode('utf-8')
                 send_reply(reply)
 
             elif len(savedMessage) == 0:
-                reply = ("NO\n").encode('utf-8')
                 #reply = ("No, message empty\n").encode('utf-8')
                 send_reply(reply)
 
             elif savedMessage.isspace() == True:
-                reply = ("NO\n").encode('utf-8')
                 #reply = ("No, message is only whitespace\n").encode('utf-8')
                 send_reply(reply)
 
             else:
-                goodMessage = True
-
-            print(messageKey + " is the message key")
-            print(savedMessage + " is the saved message")
-
-            if goodKey and goodMessage:
                 messageDict = { messageKey : savedMessage}
-                print(messageDict)
                 reply = ("OK\n").encode('utf-8')
                 send_reply(reply)
-        
+
+        ########## END PUT COMMAND ##########
+
+        ########## START GET COMMAND ##########
         elif command == "GET" or command == "get":
-            print("in the get command")
-            messageToReturnKey = fullS[3 :11]
+            reply = ("\n").encode('utf-8')
+            messageToReturnKey = get_message_key_from_fullString()
+
             if messageDict == None:
-                reply = ("\n").encode('utf-8')
-                #reply = ("No saved keys. \n").encode('utf-8')
                 send_reply(reply)
 
             else:
                 try:
                     messageToReturn = messageDict[messageToReturnKey]
                     reply = (messageToReturn + "\n").encode('utf-8')
-                except:
-                    print("not a valid key") 
-                    reply = ("\n").encode('utf-8')
+                except Exception:
+                    pass
                 finally:
                     send_reply(reply)
-
+        ########## END GET COMMAND ##########
 
         else:
-            reply = ("NO\n").encode('utf-8')
-            #reply = ("NO! Please use put or get command.\n").encode('utf-8')
             send_reply(reply)
-        #reply = ("Recieved: " + fullS).encode('utf-8')
-    except:
-        print("Something went wrong.")
-    
-
-
+        
+    except Exception:
+                    pass
